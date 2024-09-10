0301f92e62a03edaa351189f12ca23b081837a11
keisei1305 <kippop88@gmail.com>
2024-09-10 15:53:04 +0300

    rewrite directory with media and add function for display video

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import cv2 as cv
 
-img = cv.imread("imges/leopard.png")
-cv.namedWindow("Display window", cv.WINDOW_FREERATIO)
-cv.imshow("Display window", img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+def showImage(path):
+    img = cv.imread(path, cv.COLOR_BGR2GRAY)
+    cv.namedWindow("Display window", cv.WINDOW_KEEPRATIO)
+    cv.imshow("Display window", img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+def showMp4(path):
+    cap = cv.VideoCapture(path, cv.CAP_ANY)
+    while True:
+        ret, frame = cap.read()
+        if not(ret):
+            break
+        cv.imshow('frame', frame)
+        if (cv.waitKey(1)&0xFF == 27):
+            break
+
+showMp4(0)
